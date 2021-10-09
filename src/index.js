995f17565f0941c74422ef3f1e974d8a8ef3b1b8
Arnab Mukherjee <mukherjeearnab905@gmail.com>
2021-10-09 19:05:08 +0530

    Resolved BrowserRouter basename for github pages

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from "./App";
 
 ReactDOM.render(
     <React.StrictMode>
-        <BrowserRouter>
+        <BrowserRouter basename="/photography">
             <App />
         </BrowserRouter>
     </React.StrictMode>,
